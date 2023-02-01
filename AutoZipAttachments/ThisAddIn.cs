a9350e9d2fca4be456639f3e7deca1f28a769421
vqton <vuquangton@outlook.com>
2023-02-01 16:27:22 +0700

    complete

--- AutoZipAttachments/ThisAddIn.cs
+++ AutoZipAttachments/ThisAddIn.cs
@@ -1,52 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using Outlook = Microsoft.Office.Interop.Outlook;
-using Office = Microsoft.Office.Core;
 using System.IO;
-using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
 using Microsoft.Office.Interop.Outlook;
+using ICSharpCode.SharpZipLib.Zip;
+
 namespace AutoZipAttachments
 {
     public partial class ThisAddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            //Application.ItemSend += new ApplicationEvents_11_ItemSendEventHandler(CompressAttachments);
             Application.ItemSend += Application_ItemSend;
         }
-
-        private void Application_ItemSend(object Item, ref bool Cancel)
+        private bool IsCompressibleFile(Attachment attachment)
         {
-            var mail = Item as MailItem;
-            if (mail != null)
-            {
-                if (mail.Attachments.Count > 0)
-                {
-                    //var zipFile = Path.GetTempFileName() + ".zip";
-                    var tempPath = Environment.GetEnvironmentVariable("temp");
-                    var zipFile = Path.Combine(tempPath, "Archive.zip");
-                    
+            string[] compressibleExtensions = { ".doc", ".docx", ".xls", ".xlsx", ".pdf" };
+            return compressibleExtensions.Contains(Path.GetExtension(attachment.FileName));
+        }
 
-                    using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+        private bool IsSignatureImage(Attachment attachment)
+        {
+            string[] signatureImageExtensions = { ".png", ".jpg", ".jpeg" };
+            return signatureImageExtensions.Contains(Path.GetExtension(attachment.FileName));
+        }
+        private void Application_ItemSend(object Item,  ref bool cancel)
+        {
+            MailItem mailItem = Item as MailItem;
+
+            if (mailItem != null && mailItem.Attachments.Count > 0)
+            {
+                Recipient ccRecipient = mailItem.Recipients.Add("ton-vq@saigonco-op.com.vn");
+                ccRecipient.Type = (int)OlMailRecipientType.olCC;
+                ccRecipient.Resolve();
+                if (!ccRecipient.Resolved)
+                {
+                    MessageBox.Show("The recipient vuquangton@outlook.com could not be resolved.");
+                }
+
+
+                string tempFile = Path.GetTempFileName();
+                string tempPath = Path.GetDirectoryName(tempFile) + "\\" + Path.GetFileNameWithoutExtension(tempFile);
+                Directory.CreateDirectory(tempPath);
+
+                var attachmentsToCompress = mailItem.Attachments.Cast<Attachment>()
+                    .Where(attachment => !IsSignatureImage(attachment))
+                    .Where(attachment => IsCompressibleFile(attachment));
+
+                if (attachmentsToCompress.Any())
+                {
+                    string zipFile = tempPath + "\\attachments.zip";
+                    using (ZipOutputStream zipStream = new ZipOutputStream(File.Create(zipFile)))
                     {
-                        for (int i = mail.Attachments.Count; i > 0; i--)
+                        zipStream.SetLevel(9);
+                        foreach (Attachment attachment in attachmentsToCompress)
                         {
-                            var attachment = mail.Attachments[i];
-                            //var file = Path.GetTempFileName();
-                            var file = Path.Combine(tempPath, Path.GetTempFileName());
-                            attachment.SaveAsFile(file);
-                            archive.CreateEntryFromFile(file, attachment.FileName);
-                            File.Delete(file);
-                            attachment.Delete();
+                            string fileName = tempPath + "\\" + attachment.FileName;
+                            attachment.SaveAsFile(fileName);
+
+                            ZipEntry entry = new ZipEntry(attachment.FileName);
+                            entry.DateTime = DateTime.Now;
+                            entry.IsUnicodeText = true;
+                            zipStream.PutNextEntry(entry);
+
+                            using (FileStream fs = File.OpenRead(fileName))
+                            {
+                                byte[] buffer = new byte[fs.Length];
+                                fs.Read(buffer, 0, buffer.Length);
+                                zipStream.Write(buffer, 0, buffer.Length);
+                            }
                         }
                     }
-                    //mail.Attachments.Remove();
-                    mail.Attachments.Add(zipFile, OlAttachmentType.olByValue, 1, "Archive.zip");
+
+                    //mailItem.Attachments.Clear();
+
+                    for (int i = mailItem.Attachments.Count - 1; i >= 0; i--)
+                    {
+                        mailItem.Attachments.Remove(i);
+                    }
+
+
+
+                    mailItem.Attachments.Add(zipFile);
                 }
+
+                Directory.Delete(tempPath, true);
+                
             }
         }
+
+
+
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
@@ -65,7 +110,7 @@ namespace AutoZipAttachments
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
         }
-        
+
         #endregion
     }
 }
