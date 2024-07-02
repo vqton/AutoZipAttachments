462f98260601bbf9d6bab116d819e22a90c9064b
vqton <vuquangton@outlook.com>
2024-07-02 10:43:21 +0700

    Add compress method

--- AutoZipAttachments/EmailSender.cs
+++ AutoZipAttachments/EmailSender.cs
@@ -1,97 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System;
 using Outlook = Microsoft.Office.Interop.Outlook;
-using Office = Microsoft.Office.Core;
+using SharpCompress.Common;
+using SharpCompress.Writers;
+using System.Linq;
 
 namespace AutoZipAttachments
 {
     public interface IEmailSender
     {
         void AddCC(Outlook.MailItem mailItem);
-        void AddBCC(Outlook.MailItem mailItem, string groupName);
+        void AddBCC(Outlook.MailItem mailItem, string[] groupName);
+        void CompressAttachments(Outlook.MailItem mailItem, string outputPath);
+        void MoveTempDirectoryToSystemTemp(string sourcePath);
     }
     public class EmailSender : IEmailSender
-    {public interface IEmailSender
     {
-        void AddCC(Outlook.MailItem mailItem);
-        void AddBCC(Outlook.MailItem mailItem, string groupName);
-    }
+        public interface IEmailSender
+        {
+            void AddCC(Outlook.MailItem mailItem);
+            void AddBCC(Outlook.MailItem mailItem, string groupName);
+            void CompressAttachments(Outlook.MailItem mailItem, string outputPath);
+            void FormatEmail(Outlook.MailItem mailItem);
+            void MoveTempDirectoryToSystemTemp(string sourcePath);
+        }
         public void AddCC(Outlook.MailItem mailItem)
         {
-            Outlook.Recipient recipient = mailItem.Recipients.Add(mailItem.SenderEmailAddress);
+            Outlook.Recipient recipient = mailItem.Recipients.Add("ton-vq@saigonco-op.com.vn");
+
             recipient.Type = (int)Outlook.OlMailRecipientType.olCC;
             recipient.Resolve();
         }
 
-        public void AddBCC(Outlook.MailItem mailItem, string groupName)
+        public void AddBCC(Outlook.MailItem mailItem, string[] emails)
         {
-            Outlook.AddressEntry addressEntry = mailItem.Application.Session.CurrentUser.AddressEntry;
-            if (addressEntry != null && addressEntry.Type == "EX")
+            if (mailItem != null)
             {
-                Outlook.ExchangeUser manager = addressEntry.GetExchangeUser()?.GetExchangeUserManager();
-                if (manager != null)
+                foreach (string email in emails)
                 {
-                    Outlook.AddressEntries addrEntries = manager.GetDirectReports();
-                    Outlook.DistListItem distList = null;
-
-                    // Check if the group exists
-                    foreach (Outlook.AddressEntry addrEntry in addrEntries)
-                    {
-                        if (addrEntry.Name == groupName)
-                        {
-                            distList = addrEntry.GetContact() as Outlook.DistListItem;
-                            break;
-                        }
-                    }
-
-                    // If the group does not exist, create it
-                    if (distList == null)
-                    {
-                        distList = mailItem.Application.CreateItem(Outlook.OlItemType.olDistributionListItem) as Outlook.DistListItem;
-                        if (distList != null)
-                        {
-                            distList.DLName = groupName;
-                            Outlook.MailItem newMail = mailItem.Application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
-                            if (newMail != null)
-                            {
-                                Outlook.Recipient recipient = newMail.Recipients.Add("vuquangton@outlook.com");
-                                distList.AddMembers(newMail.Recipients);
-                                distList.Save();
-                            }
-                        }
-                    }
-
-                    // Add the group members to the BCC field
-                    if (distList != null)
-                    {
-                        for (int i = 1; i <= distList.MemberCount; i++)
-                        {
-                            Outlook.Recipient memberRecipient = distList.GetMember(i);
-                            if (memberRecipient != null)
-                            {
-                                Outlook.AddressEntry member = memberRecipient.AddressEntry;
-                                if (member != null)
-                                {
-                                    Outlook.Recipient bccRecipient = mailItem.Recipients.Add(member.Address);
-                                    bccRecipient.Type = (int)Outlook.OlMailRecipientType.olBCC;
-                                    bccRecipient.Resolve();
-                                }
-                            }
-                        }
-                    }
+                    Outlook.Recipient bccRecipient = mailItem.Recipients.Add(email);
+                    bccRecipient.Type = (int)Outlook.OlMailRecipientType.olBCC;
+                    bccRecipient.Resolve();
                 }
             }
         }
+        public void MoveTempDirectoryToSystemTemp(string sourcePath)
+        {
+            string systemTempPath = Path.GetTempPath();
+            string destPath = Path.Combine(systemTempPath, Path.GetFileName(sourcePath));
 
+            if (Directory.Exists(sourcePath))
+            {
+                Directory.Move(sourcePath, destPath);
+            }
+        }
+        public void CompressAttachments(Outlook.MailItem mailItem, string outputPath)
+        {
+            if (mailItem.Attachments.Count > 0)
+            {
+                // Define the temporary directory on D: drive
+                string tempDirectory = Path.Combine(@"E:\temp", Guid.NewGuid().ToString());
+                Directory.CreateDirectory(tempDirectory);
 
+                string[] excludedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff" };
 
+                try
+                {
+                    // Save each attachment to the temporary directory, excluding image files
+                    foreach (Outlook.Attachment attachment in mailItem.Attachments)
+                    {
+                        string attachmentPath = Path.Combine(tempDirectory, attachment.FileName);
+                        string extension = Path.GetExtension(attachment.FileName).ToLower();
 
+                        if (!excludedExtensions.Contains(extension))
+                        {
+                            attachment.SaveAsFile(attachmentPath);
+                        }
+                    }
 
+                    // Compress the non-image attachments into a zip archive using SharpCompress
+                    string archivePath = Path.Combine(outputPath, "attachments.zip");
 
+                    using (var archiveStream = File.OpenWrite(archivePath))
+                    using (var writer = WriterFactory.Open(archiveStream, ArchiveType.Zip, CompressionType.Deflate))
+                    {
+                        writer.WriteAll(tempDirectory, "*", SearchOption.AllDirectories);
+                    }
 
+                    // Verify that the compressed file was created and is not empty
+                    FileInfo archiveFileInfo = new FileInfo(archivePath);
+                    if (archiveFileInfo.Exists && archiveFileInfo.Length > 0)
+                    {
+                        // Remove original non-image attachments (iterate in reverse order)
+                        for (int i = mailItem.Attachments.Count; i > 0; i--)
+                        {
+                            Outlook.Attachment attachment = mailItem.Attachments[i];
+                            string extension = Path.GetExtension(attachment.FileName).ToLower();
 
+                            if (!excludedExtensions.Contains(extension))
+                            {
+                                attachment.Delete();
+                            }
+                        }
+
+                        // Attach the compressed zip file
+                        mailItem.Attachments.Add(archivePath, Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
+                    }
+                    else
+                    {
+                        throw new Exception("Compression failed or resulted in an empty archive.");
+                    }
+                }
+                finally
+                {
+                    // Cleanup temporary files
+                    if (Directory.Exists(tempDirectory))
+                    {
+                        //Directory.Delete(tempDirectory, recursive: true);
+                        Directory.Delete(@"E:\temp", recursive: true);
+                    }
+                }
+            }
+
+           
+        }
     }
 }
--- AutoZipAttachments/ThisAddIn.cs
+++ AutoZipAttachments/ThisAddIn.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
+﻿using Microsoft.Office.Interop.Outlook;
 using Outlook = Microsoft.Office.Interop.Outlook;
-using Office = Microsoft.Office.Core;
 
 namespace AutoZipAttachments
 {
@@ -14,25 +9,40 @@ namespace AutoZipAttachments
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             //Application.ItemSend += new ApplicationEvents_11_ItemSendEventHandler(CompressAttachments);
-            _emailSender = new EmailSender(); 
+            _emailSender = new EmailSender();
             Application.ItemSend += Application_ItemSend;
         }
-        
+        private void FormatEmail(MailItem mail)
+        {
+            if (mail.BodyFormat != OlBodyFormat.olFormatHTML)
+            {
+                mail.BodyFormat = OlBodyFormat.olFormatHTML;
+            }
+            string htmlBody = "<html><head><style>body { font-family: Arial; font-size: 13px; line-height: 1.5; }</style></head><body>" + mail.HTMLBody + "</body></html>";
+            mail.HTMLBody = htmlBody;
+            mail.Save();
+        }
         private void Application_ItemSend(object Item, ref bool cancel)
         {
             Outlook.MailItem mailItem = Item as Outlook.MailItem;
             if (mailItem != null)
             {
+                // Compress attachments before sending
+                string outputPath = @"E:\temp"; // Define your desired output path
+                _emailSender.CompressAttachments(mailItem, outputPath);
                 if (mailItem.Recipients.Count > 0)
                 {
+                    FormatEmail(mailItem);
                     if (mailItem.Recipients.Count > 0)
                     {
                         _emailSender.AddCC(mailItem);
 
                         // Add the backup group to the BCC field
-                        _emailSender.AddBCC(mailItem, "Backup Group");
+                        string[] bccGroup = new string[] { "tonvqsgc@outlook.com","tonqvu@gmail.com", "vuquangton@outlook.com", "vuquangton@ymail.com" };
+                        _emailSender.AddBCC(mailItem, bccGroup);
                     }
                 }
+                //_emailSender.MoveTempDirectoryToSystemTemp(outputPath);
             }
         }
 
